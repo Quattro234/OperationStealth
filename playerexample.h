99588a6aec0ac77b92ac75387db6dedf01d81b50
Quattro234 <40251270+Quattro234@users.noreply.github.com>
2018-06-14 00:18:19 +0100

    Create playerexample.h

---
+++ playerexample.h
@@ -0,0 +1,9 @@
+class player{//Matthew Fretwell
+private:
+  std::array<int, 13>playerDB;
+  
+public:
+  int pXPGain(int mEXP);
+  int pXPSplit(int mEXP,int attackNum,int defenceNum);
+//etc etc etc.....
+};
