74d28aee51ccd68bfc2e049f7afd3a55fa1b57b1
PROUSDER <40251285+PROUSDER@users.noreply.github.com>
2018-06-27 02:42:07 +0100

    Update Timer.cpp
    added visible time elapsed and defined interrupt message.

--- Timer.cpp
+++ Timer.cpp
@@ -1,24 +1,34 @@
 #include <time.h>
 #include <stdio.h>
+#include <csignal>
+#include <iostream>
 
+void signalHandler(int signum) {
+  printf("\ninterrupt signal %d received\a", signum);
+
+  exit(signum);
+}
 
 int main() {
-  clock_t t;
+
+  signal(SIGINT, signalHandler);
+  clock_t start;
+
   int waitingTime;
 
   printf("how many minutes will you waiting?   \n");
   scanf("%d",&waitingTime);
 
-  t = clock(); //record start time here
+  start = clock(); //record start time here
   waitingTime = waitingTime * CLOCKS_PER_SEC * 60;
   //calculates the number of cycles needed until required time is elapsed
-  printf("start waiting.. \n");
+  printf("start waiting.. \n\n");
 
-  while(clock() - t < waitingTime){}
+  while(clock() - start < waitingTime + 1) {
+    printf("\r %d seconds have passed.", (clock()-start)/(CLOCKS_PER_SEC));
+}
   //waits till required time has passed
-
-  printf("finished. ");
-  for(int i; i < 2; i++ ){printf(" \a");}
+  printf("\n\n\afinished. \a");
 
 return 0;
 }
